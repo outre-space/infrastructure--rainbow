f177417063d1b26ca4f7bc7065e8a0dc5bf0ba25
Sarpong <esarpong51@gmail.com>
2020-12-20 14:10:25 +0000

    Fixed config url

--- routes/api.js
+++ routes/api.js
@@ -8,18 +8,15 @@ const {SchemaBuilder} = require('../lib/SchemaBuilder')
 const path = require('path')
 
 const middlewares = async () => {
-    const schema = await new SchemaBuilder().buildFromConfig(path.resolve(process.env.CONFIG_URL))
-
-    const m1 = graphqlHTTP({schema, graphiql: true})
+    const schema = await new SchemaBuilder().buildFromConfig(process.env.CONFIG_URL)
 
     const doc = OpenAPI({schema, info: {title: 'Outre Rainbow APIs',},})
-    const m2 = useSofa({schema, onRoute: i => doc.addRoute(i, {basePath: '/api/rest',}),})
 
-    router.use('/graph', m1)
-    router.get('/rest', (req, res) => res.json(doc.get()))
-    router.use('/rest', m2)
+    router.use('/graph', graphqlHTTP({schema, graphiql: true}))
+    router.get('/rest', (req, res) =>
+        res.json(doc.get()))
+    router.use('/rest', useSofa({schema, onRoute: i => doc.addRoute(i, {basePath: '/api/rest',}),}))
 
-    return
 }
 
 
